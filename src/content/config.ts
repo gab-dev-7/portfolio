2e78b180f2921067c03f119ebf01e1a3177a1590
Gabriel Windlin <gawindlin@gmail.com>
2026-01-17 19:06:22 +0100

    post tags & ui++

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,14 +1,14 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content";
 
 const blogCollection = defineCollection({
   schema: z.object({
     title: z.string(),
     description: z.string(),
     publishDate: z.string().transform((str) => new Date(str)),
-    tags: z.array(z.string()),
+    tags: z.array(z.string()).optional(),
   }),
 });
 
 export const collections = {
-  'blog': blogCollection,
+  blog: blogCollection,
 };
